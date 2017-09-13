6c007382389a59cafb12ad642e17f58475afacad
larrykirschner <larrykirschner@gmail.com>
2017-09-13 14:28:54 -0700

    0.0.7

--- src/bindings/Binding.cs
+++ src/bindings/Binding.cs
@@ -2,19 +2,17 @@
 namespace BeatThat
 {
 	/// <summary>
-	/// Safe handle to a binding, e.g. the Binding for an IController.
+	/// A Binding is a safe reference to a listener binding that can be queried 'isBound' and used to unbind the listener.
 	///
-	/// Useful for cases where something that you would want to Bind and then later Unbind
-	/// may be a unity Component and may be destroyed when you want to unbind it.
-	///
-	/// A Binding, should survive the destruction
-	/// of its owner, and therefore is safe to hold and use.
+	/// A Binding, should survive the destruction of its owner.
+	/// 
+	/// Useful for cases where something that you would want to Bind with
+	/// the guarantee that the binding will be cleaned up if, say, the binding object is destroyed.
 	/// </summary>
 	public interface Binding
 	{
 		void Unbind();
 
 		bool isBound { get; }
-
 	}
 }
